22f1eb4db732dc0c000c243f208e989726f1e891
AlexShcherb1173 <alex181173@list.ru>
2025-06-29 18:50:35 +0300

    что то

---
+++ tests/test_masks.py
@@ -0,0 +1,5 @@
+def get_mask_card_number(card_num_in: str):
+    assert get_mask_card_number(card_num_in: str)
+
+def get_mask_acount(acount_num_in: str):
+    assert get_mask_acount(acount_num_in: str)
---
+++ tests/test_processing.py
@@ -0,0 +1,5 @@
+def test_filter_by_state(list_dict: list[dict], state_in: str = "EXECUTED"):
+    assert filter_by_state(list_dict: list[dict], state_in: str = "EXECUTED")
+
+def sort_by_date(list_dict: list[dict], reverse: bool = True):
+    assert sort_by_date(list_dict: list[dict], reverse: bool = True)
