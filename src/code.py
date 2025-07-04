8ba7058895b2c5eaac4b61dc009b561219b43bbd
AlexShcherb1173 <alex181173@list.ru>
2025-07-04 13:25:43 +0300

    работаем

---
+++ src/code.py
@@ -0,0 +1,11 @@
+import widget
+
+acount_num_in = input()
+
+print(widget.mask_account_card(acount_num_in))
+
+date_in = input()
+#
+print(widget.get_date(date_in))
+
+
---
+++ tests/code_test.py
@@ -0,0 +1,9 @@
+import src.widget
+
+acount_num_in = input()
+
+print(widget.mask_account_card(acount_num_in))
+
+date_in = input()
+#
+print(widget.get_date(date_in))
