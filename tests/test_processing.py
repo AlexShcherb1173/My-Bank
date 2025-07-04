2abb4e70e7799c1223943a9033ffca4aa92c28ff
AlexShcherb1173 <alex181173@list.ru>
2025-07-04 17:34:41 +0300

    дорабатываю

--- tests/test_processing.py
+++ tests/test_processing.py
@@ -26,31 +26,26 @@ def test_filter_by_state(list_dict1):
     assert filter_by_state(list_dict1) == [
         {'': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
         {'': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'}
-             ]
-
+        ]
 def test_filter_by_state(list_dict2):
     assert filter_by_state(list_dict2) == [
         {'': 41428829, 'state': 'EXECUTED', '': '2019-07-03T18:35:29.512364'},
         {'': 939719570, 'state': 'EXECUTED', '': '2018-06-30T02:08:58.425572'}
         ]
-
 def test_filter_by_state(list_dict3):
     assert filter_by_state(list_dict3) == [
             {'': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'}
             ]
-
 def test_filter_by_state(list_dict4):
     assert filter_by_state(list_dict4) == [
         {'id': null, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
         {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'}
         ]
-
 def test_filter_by_state(list_dict5):
     assert filter_by_state(list_dict5) == [
         {'id': 41428829, '': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
         {'id': 939719570, '': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'}
         ]
-
 def test_filter_by_state(list_dict6):
     assert filter_by_state(list_dict6) == [
         {'': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
@@ -61,13 +56,11 @@ def test_filter_by_state(list_dict6):
         {'': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
         {'': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}
         ]
-
 def test_filter_by_state(list_dict1):
     assert filter_by_state(list_dict1, 'CANCELED') == [
         {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
         {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}
         ]
-
 def test_filter_by_state(list_dict3):
     assert filter_by_state(list_dict3, 'CANCELED') == [
         {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}
@@ -77,7 +70,6 @@ def test_filter_by_state(list_dict4):
         {'id': '', 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
         {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}
         ]
-
 def test_sort_by_date(list_dict1):
     assert sort_by_date(list_dict1) == [
             {'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
