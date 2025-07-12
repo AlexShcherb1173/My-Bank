5f767f87e06e4b9adc84045beaae1acf09da42aa
AlexShcherb1173 <alex181173@list.ru>
2025-07-12 18:53:59 +0300

    ребилдинг

--- tests/test_processing.py
+++ tests/test_processing.py
@@ -1,5 +1,27 @@
-def test_filter_by_state(list_dict: list[dict], state_in: str = "EXECUTED"):
-    assert filter_by_state(list_dict: list[dict], state_in: str = "EXECUTED")
+from src_old.processing import *
 
-def sort_by_date(list_dict: list[dict], reverse: bool = True):
-    assert sort_by_date(list_dict: list[dict], reverse: bool = True)
+
+def test_filter_by_state(list_dict2):
+    assert filter_by_state(list_dict2) == [
+        {"": 41428829, "state": "EXECUTED", "": "2019-07-03T18:35:29.512364"},
+        {"": 939719570, "state": "EXECUTED", "": "2018-06-30T02:08:58.425572"},
+    ]
+
+
+def test_filter_by_state(list_dict3):
+    assert filter_by_state(list_dict3) == [{"": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"}]
+
+
+def test_filter_by_state(list_dict4):
+    assert filter_by_state(list_dict4) == [
+        {"id": null, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+        {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+    ]
+
+
+def test_filter_by_state(list_dict6):
+    assert filter_by_state(list_dict6) == [
+        {"": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+        {"": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+    ]
+
