dfa263c74a539ebbe969d73304f48703a90388fa
AlexShcherb1173 <alex181173@list.ru>
2025-07-12 17:46:17 +0300

    ребилдинг

--- tests/test_masks.py
+++
@@ -1,5 +0,0 @@
-def get_mask_card_number(card_num_in: str):
-    assert get_mask_card_number(card_num_in: str)
-
-def get_mask_acount(acount_num_in: str):
-    assert get_mask_acount(acount_num_in: str)
