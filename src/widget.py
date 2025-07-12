fb3e3eb1258a24eae6cf3f4b8c76a75e09328338
AlexShcherb1173 <alex181173@list.ru>
2025-07-12 17:36:47 +0300

    ребилдинг

--- src/widget.py
+++ src/widget.py
@@ -1,29 +1,30 @@
-def mask_account_card(card_account_number: str) -> str:
-    """Функция принимает строку с названием и номером карты или счета и возвращает строку соответсвующей
+from typing import Any
+from src_old.masks_ import *
+
+
+def mask_account_card(card_account_number: str) -> Any:
+    """Функция принимает строку с названием и номером карты или счета и возвращает строку соответсnвующей
     маски номера карты или счета"""
-    card_account_num = card_account_number.lower()
-    substring = card_account_num[:4]
+    substring = card_account_number[:4]
+    substring = substring.lower()
     if substring == "счет":
-        card_acc_num_mask = "Счет " + masks.get_mask_acount(card_account_num[5:])
+        card_acc_num_mask = "Счет " + get_mask_acount(card_account_number[5:])
     else:
         account_num = ""
         prefics = ""
-        for symbol in card_account_num:
+        for symbol in card_account_number:
             if symbol.isdigit():
                 account_num += symbol
             elif symbol.isalpha():
                 prefics += symbol
             elif symbol == " ":
                 prefics += symbol
-
-        prefics_title = prefics.title()
-        card_acc_num_mask = prefics_title + " " + masks.get_mask_card_number(account_num)
+            card_acc_num_mask = prefics + get_mask_card_number(account_num)
     return card_acc_num_mask
 
 
 def get_date(date_time_in: str) -> str:
     """Функция принимает строку установленного формата с датой и временем и вовращает строку даты
     в формате дд.мм.гггг"""
-    date_form = ""
     date_form = date_time_in[8:10] + "." + date_time_in[5:7] + "." + date_time_in[:4]
     return date_form
