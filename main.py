312d90a7ec0eb6cd008e271594daab73ae4cef21
AlexShcherb1173 <alex181173@list.ru>
2025-07-25 14:22:14 +0300

    hw13.2 progress step3

--- main.py
+++ main.py
@@ -1,25 +1,159 @@
+import csv
+import pandas as pd
+import json
+import os
 
 
-print ('''Привет! Добро пожаловать в программу работы с банковскими транзакциями.
-Выберите необходимый пункт меню:
-1. Получить информацию о транзакциях из JSON-файла
-2. Получить информацию о транзакциях из CSV-файла
-3. Получить информацию о транзакциях из XLSX-файла/n'''
-)
-inp_form = input ()
-def input_choice(i):
-    i = int(i)
-    if i == 1:
-        ch = 'json'
-    elif i == 2:
-        ch = 'csv'
-    elif i == 3:
-         ch = "xlsx"
+def read_json_file(filepath: str) -> list:
+    """Функция принимает на вход путь до JSON-файла и возвращает список словарей с данными о финансовых транзакциях.
+    Если файл пустой, содержит не список или не найден, функция возвращает пустой список."""
+    if not os.path.exists(filepath):
+        return []
+
+    try:
+        with open(filepath, "r", encoding="utf-8") as file:
+            data = json.load(file)
+            if isinstance(data, list) and all(isinstance(item, dict) for item in data):
+                return data
+            else:
+                return []
+    except (json.JSONDecodeError, IOError):
+        return []
+
+
+def transform_transaction(input_dict: dict) -> dict:
+    """Функция преобразования формата списка словарей из csv в классический вид"""
+    # Извлекаем единственный ключ и значение
+    full_header, full_values = list(input_dict.items())[0]
+
+    # Разделяем заголовки и значения по символу ";"
+    keys = full_header.split(";")
+    values = full_values.split(";")
+
+    # Преобразуем значения
+    transformed_values = []
+    for key, value in zip(keys, values):
+        if key in ("id", "amount"):
+            if value.isdigit():
+                fvalue = float(value)
+                # print(fvalue, type(fvalue))
+                transformed_values.append(fvalue)
+        else:
+            transformed_values.append(value)
+
+    # Создаем словарь из ключей и преобразованных значений
+    return dict(zip(keys, transformed_values))
+
+
+def read_transactions_from_csv(file_path: str) -> list[dict]:
+    """функция для считывания финансовых операций из CSV.
+    Принимает путь к файлу CSV в качестве аргумента.
+    Выдает список словарей с транзакциями."""
+    transactions = []
+    with open(file_path, mode="r", encoding="utf-8") as csvfile:
+        reader = csv.DictReader(csvfile)
+        for row in reader:
+            transaction = transform_transaction(row)
+            transactions.append(transaction)
+    return transactions
+
+
+def read_transactions_from_excel(file_path: str) -> list[dict]:
+    """Функция для считывания финансовых операций из Excel.
+    Принимает путь к файлу Excel в качестве аргумента.
+    Выдает список словарей с транзакциями."""
+    df = pd.read_excel(file_path)
+    transactions = df.to_dict(orient="records")
+    return transactions
+
+def filter_by_state(list_dict: list[dict], state_in: str = "EXECUTED") -> list[dict]:
+    """Функция принимает список словарей из 3х полей id, state, date и возвращает список
+    словарей-выборку по полю state"""
+    list_dict_filer_state = []
+    for account in list_dict:
+        if account['state'] == state_in:
+            list_dict_filer_state.append(account)
+    return list_dict_filer_state
+
+
+def sort_by_date(list_dict: list[dict], reverse: bool = True) -> list[dict]:
+    """Функция принимает список словарей из 3х полей id, state, date и возвращает список
+    словарей сортированных по date(назад или вперед)"""
+    if reverse:
+        list_dict_sort_by_date = sorted(list_dict, key=lambda id: id["date"], reverse=True)
     else:
-         ch = 'incorrect input'
+        list_dict_sort_by_date = sorted(list_dict, key=lambda id: id["date"], reverse=False)
+    return list_dict_sort_by_date
+
+
+def input_choice():
+    inp = input('''Привет! Добро пожаловать в программу работы с банковскими транзакциями.
+    Выберите необходимый пункт меню:
+      1. Получить информацию о транзакциях из JSON-файла
+      2. Получить информацию о транзакциях из CSV-файла
+      3. Получить информацию о транзакциях из XLSX-файла \n''')
+    while True:
+        if inp.isdigit():
+            i = int(inp)
+            if i == 1:
+                ch = 'json'
+                break
+            elif i == 2:
+                ch = 'csv'
+                break
+            elif i == 3:
+                ch = "xlsx"
+                break
+            else:
+                print('неправильный ввод, сделайте правильный выбор')
+                inp = input()
+        else:
+            print('неправильный ввод, сделайте правильный выбор')
+            inp = input()
     return ch
 
-print (f"для обработки выбран {input_choice(inp_form)}")
-print('''Введите статус, по которому необходимо выполнить фильтрацию. \n
-Доступные cтатусы: EXECUTED, CANCELED, PENDING''')
-filtre_state = input()
+os.chdir(r"C:\Users\alex_\PycharmProjects\My-Bank")
+filepath_json = r'data\operations1.json'
+filepath_csv = r'data\transactions.csv'
+filepath_xlsx = r'data\transactions_excel.xlsx'
+
+choice = input_choice()
+print (f"для обработки выбран {choice} файл")
+if choice == 'json':
+    transaction_table = read_json_file(filepath_json)
+elif choice == 'csv':
+    transaction_table = read_transactions_from_csv(filepath_csv)
+elif choice == 'xlsx':
+    transaction_table = read_transactions_from_excel(filepath_xlsx)
+
+def input_state():
+    inp = input('''Введите статус, по которому необходимо выполнить фильтрацию. \n
+    Доступные cтатусы: EXECUTED, CANCELED, PENDING \n''')
+    while True:
+        if inp.isalpha():
+            i = inp.upper()
+            if i == 'EXECUTED':
+                st = 'EXECUTED'
+                break
+            elif i == 'CANCELED':
+                st = 'CANCELED'
+                break
+            elif i == 'PENDING':
+                st = 'PENDING'
+                break
+            else:
+                print(f'Статус операции {inp} недоступен, сделайте правильный выбор')
+                inp = input()
+        else:
+            print(f'Статус операции {inp} недоступен, сделайте правильный выбор')
+            inp = input()
+    return st
+f_state_trans_tab = filter_by_state(transaction_table , input_state())
+print(f_state_trans_tab)
+#     = input('''Введите статус, по которому необходимо выполнить фильтрацию. \n
+# Доступные cтатусы: EXECUTED, CANCELED, PENDING''')
+# f_st = input()
+# if f_st.isalpha():
+#     f_state= f_st.lower()
+# else:
+
--- src/code.py
+++ src/code.py
@@ -18,7 +18,27 @@
 # @log(filename=LOG_FILE)
 # def crash(x):
 #     raise ValueError("something went wrong")
+import json
+import os
+def read_json_file(filepath: str) -> list:
+    """Функция принимает на вход путь до JSON-файла и возвращает список словарей с данными о финансовых транзакциях.
+    Если файл пустой, содержит не список или не найден, функция возвращает пустой список."""
+    print(filepath)
+    if not os.path.exists(filepath):
+        return [1]
 
+    try:
+        with open(filepath, "r", encoding="utf-8") as file:
+            data = json.load(file)
+            if isinstance(data, list) and all(isinstance(item, dict) for item in data):
+                return data
+            else:
+                return [2]
+    except (json.JSONDecodeError, IOError):
+        return [3]
+os.chdir(r'C:\Users\alex_\PycharmProjects\My-Bank')
+filepath_json = r'data\operations.json'
+print(read_json_file(filepath_json))
 # === Точка входа для запуска вручную ===
 # if __name__ == "__main__":
 #     print("Ручной запуск функций:")
@@ -50,65 +70,65 @@
 #     data = read_json_file(r"data\operations.json")
 #     print(data)
 
-sample_data = [
-    {
-        "id": 650703,
-        "state": "EXECUTED",
-        "date": "2023-09-05T11:30:32Z",
-        "amount": 16210,
-        "currency_name": "Sol",
-        "currency_code": "PEN",
-        "from": "Счет 58803664561298323391",
-        "to": "Счет 39745660563456619397",
-        "description": "Перевод организации"
-    },
-    {
-        "id": 3598919,
-        "state": "EXECUTED",
-        "date": "2020-12-06T23:00:58Z",
-        "amount": 29740,
-        "currency_name": "Peso",
-        "currency_code": "COP",
-        "from": "Discover 3172601889670065",
-        "to": "Discover 0720428384694643",
-        "description": "Перевод с карты на карту"
-    },
-]
-
-sample_data1 = [
-    {
-        "id": 1,
-        "description": "Перевод организации"
-    },
-    {
-        "id": 2,
-        "description": "Перевод с карты на карту"
-    },
-    {
-        "id": 3,
-        "description": "Перевод с карты на карту"
-    },
-    {
-        "id": 4,
-        "description": "Оплата услуг"
-    }
-]
-categories = ["Перевод с карты на карту", "Оплата услуг", "Снятие наличных"]
-# Ожидаемый результат:
-# {
-#     "Перевод с карты на карту": 2,
-#     "Оплата услуг": 1,
-#     "Снятие наличных": 0
-# }
-
-from operations import *
-import os
+# sample_data = [
+#     {
+#         "id": 650703,
+#         "state": "EXECUTED",
+#         "date": "2023-09-05T11:30:32Z",
+#         "amount": 16210,
+#         "currency_name": "Sol",
+#         "currency_code": "PEN",
+#         "from": "Счет 58803664561298323391",
+#         "to": "Счет 39745660563456619397",
+#         "description": "Перевод организации"
+#     },
+#     {
+#         "id": 3598919,
+#         "state": "EXECUTED",
+#         "date": "2020-12-06T23:00:58Z",
+#         "amount": 29740,
+#         "currency_name": "Peso",
+#         "currency_code": "COP",
+#         "from": "Discover 3172601889670065",
+#         "to": "Discover 0720428384694643",
+#         "description": "Перевод с карты на карту"
+#     },
+# ]
+#
+# sample_data1 = [
+#     {
+#         "id": 1,
+#         "description": "Перевод организации"
+#     },
+#     {
+#         "id": 2,
+#         "description": "Перевод с карты на карту"
+#     },
+#     {
+#         "id": 3,
+#         "description": "Перевод с карты на карту"
+#     },
+#     {
+#         "id": 4,
+#         "description": "Оплата услуг"
+#     }
+# ]
+# categories = ["Перевод с карты на карту", "Оплата услуг", "Снятие наличных"]
+# # Ожидаемый результат:
+# # {
+# #     "Перевод с карты на карту": 2,
+# #     "Оплата услуг": 1,
+# #     "Снятие наличных": 0
+# # }
+#
+# from operations import *
+# import os
 from deepdiff import DeepDiff
 
 #print(process_bank_search(sample_data, ''))
-print(process_bank_operations(sample_data1, categories))
-
-os.chdir(r"C:\Users\alex_\PycharmProjects\My-Bank")
+# print(process_bank_operations(sample_data1, categories))
+#
+# os.chdir(r"C:\Users\alex_\PycharmProjects\My-Bank")
 # print ('Excel')
 # print(read_transactions_from_excel(r'data\transactions_excel.xlsx'))
 # print('\n')
